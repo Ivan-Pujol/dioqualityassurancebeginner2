30a7452c34676e300a7c90e301c6797294a7e258
Ivan-Pujol <ivan.ivanovich@hotmail.com>
2021-08-23 14:29:46 -0300

    uploading the last files

--- src/main/java/com/github/andrelugomes/CitiesApplication.java
+++ src/main/java/com/github/andrelugomes/CitiesApplication.java
@@ -1,13 +1,11 @@
 package com.github.andrelugomes;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
 
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class CitiesApplication {
 
   public static void main(final String[] args) {
--- src/main/java/com/github/andrelugomes/exceptions/AppExceptionHandler.java
+++ src/main/java/com/github/andrelugomes/exceptions/AppExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.github.andrelugomes.exceptions;
 
-import com.github.andrelugomes.cities.resources.DistanceResource;
-import jdk.internal.org.jline.utils.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -12,12 +10,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+//@ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
     Logger log = LoggerFactory.getLogger(AppExceptionHandler.class);
 
 
-    @ExceptionHandler(Exception.class)
+    //@ExceptionHandler(Exception.class)
     public ResponseEntity HandleANyException(Exception e, WebRequest request){
         log.info("Handling the exception");
         String erro = e.getLocalizedMessage();
